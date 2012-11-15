eecf86641f2d0b53935558958bdeb5aba257d568
Jakob Viskum Damgaard <jakob_damgaard@hotmail.com>
2012-11-15 21:28:32 +0100

    v0.1.0

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 /**
  * Module dependencies.
  */
@@ -12,15 +11,22 @@ var express = require('express')
 var app = express();
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
+  app.set('port', process.env.PORT || 8080);
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
+  app.set('view engine', 'ejs');
+  app.use(express.compress());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
-  app.use(require('less-middleware')({ src: __dirname + '/public' }));
+  app.use(require('less-middleware')({
+		src: __dirname + '/less',
+		dest: __dirname + '/public/css',
+		prefix: '/css',
+		compress: true,
+		optimization: 2,
+		debug: true
+	}));
   app.use(express.static(path.join(__dirname, 'public')));
 });
 
