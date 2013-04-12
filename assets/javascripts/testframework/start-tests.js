9c4b39fe1bd5093b87ed2107c6b39a49d0dd7fef
Jakob Viskum Damgaard <jakob_damgaard@hotmail.com>
2013-04-12 09:37:01 +0200

    Test in browser

---
+++ assets/javascripts/testframework/start-tests.js
@@ -0,0 +1,3 @@
+mocha.checkLeaks();
+mocha.globals(['jQuery']);
+mocha.run();
---
+++ assets/javascripts/testframework/test/expect-mocha.js
@@ -0,0 +1,5 @@
+describe('Mocha and Expectjs', function () {
+  it('should be running', function () {
+    expect(true).to.be(true);
+  });
+});
